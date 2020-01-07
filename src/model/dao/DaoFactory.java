4761705205ef0e1b5cdbf3e80a36cf45fc2e0433
Toshiro <toshiro.lyn@gmail.com>
2020-01-07 18:03:22 -0300

    findByID implementation / correcting SellerDao and SellerDaoJDBC

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DB.getConnection());
 
 		// SellerDao sellerDao = DaoFactory.createSellerDao();
 		// Desta forma o programa não conhece a implementação, mas somente a interface
