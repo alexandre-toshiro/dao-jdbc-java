feea9ab94aa912172e6d209ff3a6330801e2415f
Toshiro <toshiro.lyn@gmail.com>
2020-01-07 20:58:29 -0300

    findAll implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -89,8 +89,46 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+"FROM seller INNER JOIN department "
+					+"ON seller.DepartmentId = department.Id "
+					+"ORDER BY Name"
+					);
+
+			
+			rs = st.executeQuery();
+			
+			List<Seller> list = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap<>();
+			
+			while(rs.next()) {// while, pois podem ter várias pessoas por departamento.
+				
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				//Verifica pelo ID se o dep já foi instânciado, caso não tenha sido, retornará nulo para
+				// a var "dep"
+				
+				if(dep == null) {// se dep for nulo
+					dep = instantiateDepartment(rs);// instância o department
+					map.put(rs.getInt("DepartmentId"), dep);// guarda esse deparment no map.
+					// Com essa lógica, teremos intânciado apenas um departamento e um ou mais
+					// vendedores apontando pra ele.
+				}
+				
+				Seller obj = instantiateSeller(rs, dep);
+				list.add(obj);
+				
+			}
+			return list;
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 	@Override
